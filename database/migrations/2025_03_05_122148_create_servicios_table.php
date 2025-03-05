5458e7c1688f841e387903038cd4f9bfcf8123aa
Alejandro <136236926+AlejandroG20@users.noreply.github.com>
2025-03-05 13:36:03 +0100

    Migraciones de Habitacion y Servicios

---
+++ database/migrations/2025_03_05_122148_create_servicios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->boolean('disponible')->default(true);
+            $table->foreignId('estancia_id')->nullable()->constrained('estancias')->onDelete('cascade');
+            $table->foreignId('reserva_id')->nullable()->constrained('reservas')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
