86c32411ab163516d1c4ba0a8dbbaeec0de0a36e
Alejandro <136236926+AlejandroG20@users.noreply.github.com>
2025-03-10 09:50:08 +0100

    Hasheo 2

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -9,17 +9,31 @@ class Usuario extends Authenticatable
 {
     use Notifiable;
 
+    // Los atributos que se pueden llenar de manera masiva
     protected $fillable = [
         'nombre',
         'email',
         'contraseña',
         'dni',
         'telefono',
-        'is_admin',
+        'is_admin', // Asegúrate de tener este campo en tu base de datos si planeas usarlo
     ];
 
+    // Los atributos que deben ser ocultos cuando se convierten en un array o JSON
     protected $hidden = [
-        'contraseña',
+        'contraseña', // Es importante que el campo contraseña esté oculto
         'remember_token',
     ];
+
+    // Asegúrate de que las contraseñas se hasheen automáticamente al crearlas o actualizarlas
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($usuario) {
+            if ($usuario->isDirty('contraseña')) {
+                $usuario->contraseña = bcrypt($usuario->contraseña);
+            }
+        });
+    }
 }
